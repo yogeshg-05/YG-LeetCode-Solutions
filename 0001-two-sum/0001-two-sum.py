aa9242c853ec6abb2ba04362c14a1450d12b96f3
Yogesh Gate <mr.yogeshg05@gmail.com>
2026-08-25 20:04:31 +0530

    Time: 2 ms (56.99%), Space: 13.3 MB (36.58%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[nums[i]] = i
